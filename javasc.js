1a16024208474e316ac3bce6bf66fde3a9120600
Gaurav Joshi <gauravjoshiaa1@gmail.com>
2024-03-30 21:15:58 +0530

    now i had fixed all the problem

--- javasc.js
+++ javasc.js
@@ -2,22 +2,22 @@ let boxes=document.querySelectorAll(".btn");
 let h1=document.querySelector("#h1");
 let turnO=true;
 console.log(boxes);
-boxes.forEach((btn)=>{
-  btn.addEventListener("click",()=>{
+boxes.forEach((button)=>{
+  button.addEventListener("click",()=>{
     if(turnO){
-      btn.innerText="X"
-      btn.style.backgroundColor = "yellow";
-      btn.disabled=true;
+      button.innerText="X"
+      button.style.backgroundColor = "yellow";
+      button.disabled=true;
       turnO=false;
     }else{
-      btn.innerText="O";
-      btn.style.backgroundColor="red";
-      btn.disabled=true;
+      button.innerText="O";
+      button.style.backgroundColor="red";
+      button.disabled=true;
       turnO=true;
     }
     let x,y,z;
     if(boxes[x=0].innerText=="X" && boxes[y=1].innerText=="X" && boxes[z=2].innerText=="X" || boxes[x=0].innerText=="X" && boxes[y=3].innerText=="X" && boxes[z=6].innerText=="X" || boxes[x=0].innerText=="X" && boxes[y=4].innerText=="X" && boxes[z=8].innerText=="X" || boxes[x=3].innerText=="X" && boxes[y=4].innerText=="X" && boxes[z=5].innerText=="X" || boxes[x=6].innerText=="X" && boxes[y=7].innerText=="X" && boxes[z=8].innerText=="X" || boxes[x=1].innerText=="X" && boxes[y=4].innerText=="X" && boxes[z=7].innerText=="X" || boxes[x=2].innerText=="X" && boxes[y=5].innerText=="X" && boxes[z=8].innerText=="X" ||  boxes[x=2].innerText=="X" && boxes[y=4].innerText=="X" && boxes[z=6].innerText=="X" ){
-
+      console.log("in X : x = ", x ," y =  ",y," z = ",z);
       boxes[x].style.backgroundColor="pink";
       boxes[y].style.backgroundColor="pink";
       boxes[z].style.backgroundColor="pink";
@@ -26,6 +26,7 @@ boxes.forEach((btn)=>{
       document.querySelector("#popp").innerText="You can  reset and play again";
       popup.style.display="block";
     }else if(boxes[x=0].innerText=="O" && boxes[y=1].innerText=="O" && boxes[z=2].innerText=="O" || boxes[x=0].innerText=="O" && boxes[y=3].innerText=="O" && boxes[z=6].innerText=="O" || boxes[x=0].innerText=="O" && boxes[y=4].innerText=="O" && boxes[z=8].innerText=="O" || boxes[x=3].innerText=="O" && boxes[y=4].innerText=="O" && boxes[z=5].innerText=="O" || boxes[x=6].innerText=="O" && boxes[y=7].innerText=="O" && boxes[z=8].innerText=="O" || boxes[x=1].innerText=="O" && boxes[y=4].innerText=="O" && boxes[z=7].innerText=="O" || boxes[x=2].innerText=="O" && boxes[y=5].innerText=="O" && boxes[z=8].innerText=="O" || boxes[x=2].innerText=="O" && boxes[y=4].innerText=="O" && boxes[z=6].innerText=="O"  ){
+      console.log("in O : x = ", x ," y =  ",y," z = ",z);
       boxes[x].style.backgroundColor="pink";
       boxes[y].style.backgroundColor="pink";
       boxes[z].style.backgroundColor="pink";
@@ -33,7 +34,22 @@ boxes.forEach((btn)=>{
       document.querySelector("#poph2").innerText="Player O is the Winner";
       document.querySelector("#popp").innerText="You can  reset and play again";
       popup.style.display="block";
+    }else{
+      let tie=true;
+      for(let box of boxes){
+        if(box.innerText==""){
+          tie=false;
+          break;
+        }
+      }
+      if(tie){
+        var popup = document.querySelector("#popupContainer");
+      document.querySelector("#poph2").innerText="Tie!🤔";
+      document.querySelector("#popp").innerText="You can  reset and play again";
+      popup.style.display="block";
+      }
     }
+
   })
 } )
 
